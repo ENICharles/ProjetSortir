2e797af4555257b0631456edc69c95336d052dbf
ENICharles <charles.ravinet2021@campus-eni.fr>
2022-04-11 16:51:28 +0200

    ADD: - CRUD All

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Campus;
+use App\Entity\City;
+use App\Entity\Event;
+use App\Entity\Localisation;
+use App\Entity\State;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -27,5 +32,10 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('State', 'fas fa-list', State::class);
+        yield MenuItem::linkToCrud('Localisation', 'fas fa-list', Localisation::class);
+        yield MenuItem::linkToCrud('Event', 'fas fa-list', Event::class);
+        yield MenuItem::linkToCrud('City', 'fas fa-list', City::class);
+        yield MenuItem::linkToCrud('Campus', 'fas fa-list', Campus::class);
     }
 }
