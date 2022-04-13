836ca215a22e3aefafef0bb51c1b219405bae620
spagenaud2021 <sarah.pagenaud@outlook.fr>
2022-04-13 14:37:10 +0200

    add update event , fonction delete (à vérifier), maj formulaires

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Form\UpdateEventType;
 use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,7 +20,7 @@ class EventController extends AbstractController
         return $this->render('event/index.html.twig', ['controller_name' => 'EventController',]);
     }
 
-    #[Route('/detail/{id}', name: '_detail')]
+    #[Route('/detail/{id}', name: '_detail',requirements: ["id" => "\d+"])]
     public function detail(
         EventRepository $eventRepository,
         Event $event
@@ -26,4 +29,43 @@ class EventController extends AbstractController
         return $this->render('event/detail.html.twig',
         compact('event'));
     }
+
+    #[Route('/update/{id}', name: '_update',requirements: ["id" => "\d+"])]
+
+    public function update(
+        Event $event,
+        EventRepository $eventRepository,
+        EntityManagerInterface $entityManager,
+        Request $request,
+
+    ): Response
+    {
+        $eventForm = $this->createForm(UpdateEventType::class,$event);
+
+        $eventForm->handleRequest($request);
+
+        if ($eventForm->isSubmitted() && $eventForm->isValid()){
+            $entityManager->persist($event);
+            $entityManager->flush();
+            return $this->redirectToRoute('main_index');
+        }
+
+        return $this->renderForm('event/update.html.twig',
+            compact('eventForm'));
+    }
+/*
+ * todo / A vérifier avec le nouveau formulaire
+ */
+//    #[Route('/delete/{id}', name: '_delete',requirements: ["id" => "\d+"])]
+//    public function delete(
+//        Event $event,
+//        EventRepository $eventRepository,
+//        EntityManagerInterface $entityManager,
+//
+//    ): Response
+//    {
+//        $entityManager->remove($event);
+//        $entityManager->flush();
+//        return $this->redirectToRoute('main_index');
+//    }
 }
