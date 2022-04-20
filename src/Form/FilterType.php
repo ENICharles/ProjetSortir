af77426a2581c3320f9d6345a221aebdb2088dc4
LrxCyril <cyril.leroux.ca@gmail.com>
2022-04-20 15:15:24 +0200

    LRX | UPDATE CSS MAJ FilterType.php problème de syntaxe

--- src/Form/FilterType.php
+++ src/Form/FilterType.php
@@ -4,17 +4,12 @@ namespace App\Form;
 
 use App\Entity\Campus;
 use App\Entity\Filter;
-use EasyCorp\Bundle\EasyAdminBundle\Form\Filter\Type\BooleanFilterType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use App\Entity\Product;
