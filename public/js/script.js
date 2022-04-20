fa21149018c8704fa2e03aba46ac033619ca8430
ENICharles <charles.ravinet2021@campus-eni.fr>
2022-04-20 09:59:02 +0200

    comit intermédiaire

--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,35 @@ function selectDate()
     document.getElementById('dateStart').disabled = !document.getElementById('dateStart').disabled;
     document.getElementById('dateEnd').disabled = !document.getElementById('dateEnd').disabled;
 }
+
+function getDataLocalisation()
+{
+    let nomLieu = document.getElementById('event_localisation_name').value;
+    let req  = "";
+
+    // alert('hgfkhfg');
+
+    $(document).ready(function ()
+    {
+        $.ajax(
+            {
+                url: "/event/getDataLocalisation",
+                method: "GET",
+                dataType: "json",
+            })
+
+            .done(function(response)
+            {
+                let data = JSON.parse(response);
+                //let data = JSON.stringify(response);
+
+                console.log(response);
+                // document.getElementById('event_description').value = data[0].name;
+            })
+
+            .fail(function(error)
+            {
+                //alert("pas de retour du serveur");
+            });
+    });
+}
